065972ab602adf7fdf1b2bfa89b34c373ed6b04c
Jefferson Brasilino <jefferson.wendhel@gmail.com>
2026-05-09 19:27:11 -0300

    feat: agentic refactoring

--- internal/user/infrastructure/database/gorm_model.go
+++ internal/user/infrastructure/database/gorm_model.go
@@ -1,76 +1,90 @@
 package database
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Person struct {
 	gorm.Model
+	Uuid      string           `gorm:"column:uuid;not null"`
 	Name      string           `gorm:"column:name;not null"`
 	Document  string           `gorm:"column:document;not null"`
 	BirthDate string           `gorm:"column:birth_date;not null"`
 	Users     []Users          `gorm:"foreignKey:PersonId"`
 	Contacts  []PersonContacts `gorm:"foreignKey:PersonId"`
+	Status    int              `gorm:"column:status;not null; default:1"`
 }
 
 type PersonContacts struct {
 	gorm.Model
+	Uuid          string `gorm:"column:uuid;not null"`
 	Contact       string `gorm:"column:contact;not null"`
 	Main          bool   `gorm:"column:main;not null; default:false"`
 	PersonId      uint   `gorm:"column:person_id;not null"`
 	Person        Person
 	ContactTypeId uint `gorm:"column:person_contact_type_id;not null"`
 	ContactType   PersonContactsType
+	Status        int `gorm:"column:status;not null; default:1"`
 }
 
 type PersonContactsType struct {
 	gorm.Model
+	Uuid           string           `gorm:"column:uuid;not null"`
 	Name           string           `gorm:"column:name;not null"`
 	PersonContacts []PersonContacts `gorm:"foreignKey:ContactTypeId"`
+	Status         int              `gorm:"column:status;not null; default:1"`
 }
 
 type Users struct {
 	gorm.Model
-	Username         string        `gorm:"column:username;not null"`
-	Password         string        `gorm:"column:password;not null"`
-	VerificationCode string        `gorm:"column:verification_code"`
-	UserGroups       []UsersGroups `gorm:"many2many:user_group_users;joinForeignKey:user_id;joinReferences:user_group_id"`
-	PersonId         uint          `gorm:"column:person_id;not null"`
+	Uuid             string `gorm:"column:uuid;not null"`
+	Username         string `gorm:"column:username;not null"`
+	Password         string `gorm:"column:password;not null"`
+	VerificationCode string `gorm:"column:verification_code"`
+	PersonId         uint   `gorm:"column:person_id;not null"`
 	Person           Person
-	Devices          []UsersDevice `gorm:"foreignKey:UserId"`
-}
-
-type UsersDevice struct {
-	gorm.Model
-	UserId   uint `gorm:"column:user_id;not null"`
-	User     Users
-	DeviceId string `gorm:"column:device_id;not null"`
-}
-
-type UserGroupsPermissions struct {
-	gorm.Model
-	ApiRoutApplicationId uint   `gorm:"column:api_route_application_id;not null"`
-	Action               string `gorm:"column:action;not null"`
-	UserGroupId          uint   `gorm:"column:user_group_id;not null"`
-	UserGroup            UsersGroups
-}
-
-type UserGroupUser struct {
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
-	DeletedAt     gorm.DeletedAt `gorm:"index"`
-	Main          bool           `gorm:"column:main;not null; default:false"`
-	UsersID       uint           `gorm:"column:user_id;primaryKey"`
-	UsersGroupsID uint           `gorm:"column:user_group_id;primaryKey"`
+	Status           int `gorm:"column:status;not null; default:1"`
+	// UserGroups       []UsersGroups `gorm:"many2many:user_group_users;joinForeignKey:user_id;joinReferences:user_group_id"`
+	Devices         []UsersDevice   `gorm:"foreignKey:UserId"`
+	UserGroupsUsers []UserGroupUser `gorm:"foreignKey:UserId"` //for create/update only need change custon fields.
 }
 
 type UsersGroups struct {
 	gorm.Model
-	Name        string                  `gorm:"column:name;not null"`
-	Users       []Users                 `gorm:"many2many:user_group_users;joinForeignKey:user_group_id;joinReferences:user_id"`
-	Permissions []UserGroupsPermissions `gorm:"foreignKey:UserGroupId"`
+	Uuid string `gorm:"column:uuid;not null"`
+	Name string `gorm:"column:name;not null"`
+	//Users       []Users                 `gorm:"many2many:user_group_users;joinForeignKey:user_group_id;joinReferences:user_id"`
+	Permissions     []UserGroupsPermissions `gorm:"foreignKey:UserGroupId"`
+	Status          int                     `gorm:"column:status;not null; default:1"`
+	UserGroupsUsers []UserGroupUser         `gorm:"foreignKey:UserGroupId"`
+}
+
+type UserGroupUser struct {
+	gorm.Model
+	Uuid        string `gorm:"column:uuid;not null"`
+	Main        bool   `gorm:"column:main;not null; default:false"`
+	UserId      uint   `gorm:"column:user_id"`
+	UserGroupId uint   `gorm:"column:user_group_id"`
+	Status      int    `gorm:"column:status;not null; default:1"`
+}
+
+type UsersDevice struct {
+	gorm.Model
+	Uuid     string `gorm:"column:uuid;not null"`
+	UserId   uint   `gorm:"column:user_id;not null"`
+	User     Users
+	DeviceId string `gorm:"column:device_id;not null"`
+	Status   int    `gorm:"column:status;not null; default:1"`
+}
+
+type UserGroupsPermissions struct {
+	gorm.Model
+	Uuid                 string `gorm:"column:uuid;not null"`
+	ApiRoutApplicationId uint   `gorm:"column:api_route_application_id;not null"`
+	Action               string `gorm:"column:action;not null"`
+	UserGroupId          uint   `gorm:"column:user_group_id;not null"`
+	UserGroup            UsersGroups
+	Status               int `gorm:"column:status;not null; default:1"`
 }
 
 func (Users) TableName() string {
